fbb82a952140b1aead131689f9e4cd1f19d0dcec
Sandrine <sandrineblandamour@gmail.com>
2023-05-23 19:01:47 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+//document.addEventListener("DOMContentLoaded", () => {
+//}); 
+
+//const btnMenu = document.querySelector(".logo-menu");
+//const menu = document.querySelector(".liste-nav");
+
+//btnMenu.addEventListener("click", () => {
+    menu.classList.toggle("active");
+//});
+
+// action pour qu'au click sur un item, ça se ferme
+
+//const allLinks = document.querySelectorAll(".item-nav");
+
+// au click je veux aussi faire toggle
+//forEach est une fonction qui va s'exécuter pour chacun des items
+
+//allLinks.forEach((item) => {
+    //item.addEventListener("click", () => {
+        //menu.classList.toggle("active");
+    //});
+//});
